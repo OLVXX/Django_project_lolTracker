56e7dc4bacd3e5d16027c7471294e7c099ba52b0
Olaf <oli.madryt@gmail.com>
2025-03-10 18:48:31 +0100

    Update build script with pip upgrade

--- match_analyzer/build.sh
+++ match_analyzer/build.sh
@@ -2,9 +2,15 @@
 # exit on error
 set -o errexit
 
+# Upgrade pip first
+python -m pip install --upgrade pip
+
 # Install dependencies
 pip install -r requirements.txt
 
+# Create static directory if it doesn't exist
+mkdir -p staticfiles
+
 # Run Django commands
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
