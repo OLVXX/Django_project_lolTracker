0f05b1f0cd53421675eaa73ba889148130b0431e
Olaf <oli.madryt@gmail.com>
2025-03-10 19:56:58 +0100

    Fix analysis app import

--- match_analyzer/match_analyzer/settings.py
+++ match_analyzer/match_analyzer/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',  # Add whitenoise
-    'analysis.apps.AnalysisConfig',
+    'analysis',  # Change this line from 'analysis.apps.AnalysisConfig'
     'accounts',
 ]
 
