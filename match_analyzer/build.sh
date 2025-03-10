0f878706b23e2b5c6e1c2553664c5656e0618275
Olaf <oli.madryt@gmail.com>
2025-03-10 18:44:04 +0100

    Add build script

--- match_analyzer/build.sh
+++ match_analyzer/build.sh
@@ -5,9 +5,6 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Navigate to the directory containing manage.py
-cd match_analyzer
-
 # Run Django commands
 python manage.py collectstatic --no-input
 python manage.py migrate
