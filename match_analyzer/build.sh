778bbdbe2bbeaa0560e7cfbad53c1031b72dbcab
Olaf <oli.madryt@gmail.com>
2025-03-10 18:42:01 +0100

    Fix deployment configuration

--- match_analyzer/build.sh
+++ match_analyzer/build.sh
@@ -5,8 +5,9 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Collect static files
-python manage.py collectstatic --no-input
+# Navigate to the directory containing manage.py
+cd match_analyzer
 
-# Run migrations
+# Run Django commands
+python manage.py collectstatic --no-input
 python manage.py migrate
